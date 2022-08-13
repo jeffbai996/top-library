5468971c8f0061161b3d2ac3c936a4164ac4840f
jeffbai996 <j.bai@wustl.edu>
2022-08-13 16:48:45 -0700

    Added comments to script file

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// create array for book objects
 let myLibrary = [];
 
+// create 2 placeholder books to populate array
 let book1 = new Book("The Hunger Games", "Suzanne Collins", "374", false);
 addBookToLibrary(book1);
 let book2 = new Book("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "870", false)
 addBookToLibrary(book2);
 
+// constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,14 +15,13 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// push a book to myLibrary array
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-let form = document.querySelector("form");
-
+// function to handle form submission
 function handleSubmit() {
-    console.log("Firing!");
     title = document.getElementById("title").value;
     author = document.getElementById("author").value;
     pages = document.getElementById("pages").value;
@@ -31,8 +33,10 @@ function handleSubmit() {
     printBooks();
 }
 
+// get book-list unordered list element
 const bookList = document.getElementById("book-list")
 
+// function to update book list element with each submit
 function printBooks() {
     bookList.innerHTML = ""; 
     myLibrary.forEach((book) => {
@@ -41,4 +45,5 @@ function printBooks() {
     })
 }
 
+// print placeholder books
 printBooks();
