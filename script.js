5103754301df1706903c550c4c9fc5c546509239
jeffbai996 <j.bai@wustl.edu>
2023-01-18 15:24:36 -0800

    Remove modal handling code in script

--- script.js
+++ script.js
@@ -57,21 +57,3 @@ document.getElementById("new-book").addEventListener("click", () => {
 
 // print placeholder books
 displayBooks();
-
-//modal handling
-const modals = document.querySelectorAll('[data-modal]');
-
-modals.forEach(function (trigger) {
-  trigger.addEventListener('click', function (event) {
-    event.preventDefault();
-    const modal = document.getElementById(trigger.dataset.modal);
-    modal.classList.add('open');
-    const exits = modal.querySelectorAll('.modal-exit');
-    exits.forEach(function (exit) {
-      exit.addEventListener('click', function (event) {
-        event.preventDefault();
-        modal.classList.remove('open');
-      });
-    });
-  });
-});
